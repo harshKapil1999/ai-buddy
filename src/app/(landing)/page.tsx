6b9d54e5cd03f2840ad1d769b032015b280ddbe3
Harsh Kapil <kapilharsh1999@gmail.com>
2024-05-24 23:24:51 +0530

    Ai Buddy - project setup

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <main className="flex flex-col items-center w-full h-full min-h-screen">
+            <h1 className="text-3xl font-bold p-4">Ai Buddy</h1>
+        </main>
+    );
+}
